9ff2fc7455f0ccad7985d9c5deb18e91a7e8613e
Emanuell Amorim <amorimnuell@gmail.com>
2019-11-25 18:24:49 -0400

    Create the InteractionRaycaster
    * This class will be responsible for sending input events to the objects
    in the scene.
    * I created an interface(IInteractable) that implements
    some basics input interactions.
    * All interactable objects will be placed in a layer called 'Interactable'.
    * Created a CardController class that will be responsible for the
    interactable cards in the game.
    
    Signed-off-by: Emanuell Amorim <amorimnuell@gmail.com>

---
+++ Assets/Scripts/CardController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardController : MonoBehaviour, IInteractable
+{
+    public void OnClick()
+    {
+        Debug.Log(name + " OnClick");
+    }
+
+    public void OnTouchDown()
+    {
+        Debug.Log(name + " OnTouchDown");
+    }
+
+    public void OnTouchUp()
+    {
+        Debug.Log(name + " OnTouchUp");
+    }
+}
---
+++ Assets/Scripts/InteractionRaycaster.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionRaycaster : MonoBehaviour
+{
+    [SerializeField] private Camera _camera = default;
+    [SerializeField] private LayerMask _rayMask = default;
+
+    private const float ClickThreshold = 0.3f;
+
+    private Dictionary<Collider, IInteractable> _savedInteractables = new Dictionary<Collider, IInteractable>();
+    private IInteractable _currentInteractable;
+    private float _touchDownTime;
+    private RaycastHit[] _hit = new RaycastHit[1];
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            HandleTouchDown();
+        else if (Input.GetMouseButtonUp(0))
+            HandleTouchUp();
+    }
+
+    private void HandleTouchDown()
+    {
+        _touchDownTime = Time.unscaledTime;
+
+        var interactable = GetInteractable();
+        if (interactable != null)
+        {
+            if (_currentInteractable != interactable)
+                _currentInteractable = interactable;
+
+            _currentInteractable.OnTouchDown();
+        }
+    }
+
+    private void HandleTouchUp()
+    {
+        var interactable = GetInteractable();
+        if (interactable != null)
+        {
+            if (_currentInteractable == interactable)
+            {
+                _currentInteractable.OnTouchUp();
+
+                if (Time.unscaledTime - _touchDownTime <= ClickThreshold)
+                    _currentInteractable.OnClick();
+            }
+        }
+
+        _currentInteractable = null;
+    }
+
+    private IInteractable GetInteractable()
+    {
+        var ray = _camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.RaycastNonAlloc(ray, _hit, Mathf.Infinity, _rayMask) > 0)
+        {
+            if (_savedInteractables.ContainsKey(_hit[0].collider))
+                return _savedInteractables[_hit[0].collider];
+
+            var interactable = _hit[0].collider.GetComponent<IInteractable>();
+            if (interactable != null)
+            {
+                _savedInteractables.Add(_hit[0].collider, interactable);
+                return interactable;
+            }
+        }
+
+        return null;
+    }
+}
