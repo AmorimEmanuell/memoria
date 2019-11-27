a01612d324ed386a37471aab0b0123a69273fcd1
Emanuell Amorim <amorimnuell@gmail.com>
2019-11-27 09:19:39 -0400

    Implement the CardGridBuilder
    Signed-off-by: Emanuell Amorim <amorimnuell@gmail.com>

---
+++ Assets/Scripts/CardGridBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardGridBuilder : MonoBehaviour
+{
+    [SerializeField] private GridLayoutGroup _grid = default;
+    [SerializeField] private int _rows = 3;
+    [SerializeField] private int _columns = 4;
+
+    private int _numberOfCells;
+    private List<Transform> _positions = new List<Transform>();
+
+    private void Awake()
+    {
+        CreateGrid(_rows, _columns);
+    }
+
+    public void CreateGrid(int rows, int columns)
+    {
+        _grid.constraintCount = columns;
+
+        _numberOfCells = rows * columns;
+        var gridTransform = _grid.transform;
+
+        for (var i = 0; i < _numberOfCells; i++)
+        {
+            var pos = new GameObject("pos" + i, typeof(RectTransform));
+            pos.transform.SetParent(gridTransform);
+            _positions.Add(pos.transform);
+        }
+    }
+}
