05cb8d5c18d9009f3dcbcdef0692d8ab14431a02
Emanuell Amorim <amorimnuell@gmail.com>
2019-11-27 18:45:22 -0400

    Create the CardDataCollection
    This will work as a database for all the cards in the game. I created a
    CardData scriptableobject to contain the information of a card(id,
    texture, etc...).
    
    Signed-off-by: Emanuell Amorim <amorimnuell@gmail.com>

---
+++ Assets/Scripts/CardDataCollection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDataCollection : MonoBehaviour
+{
+    [SerializeField] private CardData[] _data = default;
+
+    public CardData[] GetData()
+    {
+        return _data;
+    }
+
+    public List<int> GetIds()
+    {
+        var ids = new List<int>();
+
+        for (var i = 0; i < _data.Length; i++)
+            ids.Add(_data[i].Id);
+
+        return ids;
+    }
+
+    public Texture GetTexture(int cardId)
+    {
+        for (var i = 0; i < _data.Length; i++)
+        {
+            if (_data[i].Id == cardId)
+                return _data[i].Texture;
+        }
+
+        return null;
+    }
+}
