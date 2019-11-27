6c8afe778d7f2a2647383439ca891b9d20ee3ca8
Emanuell Amorim <amorimnuell@gmail.com>
2019-11-27 18:45:22 -0400

    Create the CardDataCollection
    This will work as a database for all the cards in the game. I created a
    CardData scriptableobject to contain the information of a card(id,
    texture, etc...).
    
    Signed-off-by: Emanuell Amorim <amorimnuell@gmail.com>

---
+++ Assets/Scripts/CardData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card Data", menuName = "Card Data")]
+public class CardData : ScriptableObject
+{
+    [SerializeField] private int _id = default;
+    [SerializeField] private Texture _texture = default;
+
+    public int Id => _id;
+    public Texture Texture => _texture;
+}
