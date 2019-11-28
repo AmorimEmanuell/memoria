a453f5a93d6e873a9f5930eb114b159f21a93026
Emanuell Amorim <amorimnuell@gmail.com>
2019-11-28 11:40:17 -0400

    Create the ObjectPooler
    For now it will manage the pool of cards and grid cells.
    
    Signed-off-by: Emanuell Amorim <amorimnuell@gmail.com>

---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PoolItemType
+{
+    Card,
+    Cell
+}
+
+public class ObjectPooler : Singleton<ObjectPooler>
+{
+    [SerializeField] private List<ObjectPoolItem> _objectsToPool;
+
+    private Dictionary<PoolItemType, List<GameObject>> _pools = new Dictionary<PoolItemType, List<GameObject>>();
+
+    private void Awake()
+    {
+        for (var i = 0; i < _objectsToPool.Count; i++)
+            _pools.Add(_objectsToPool[i].ItemType, new List<GameObject>());
+    }
+
+    public GameObject Retrieve(PoolItemType itemType)
+    {
+        var pooledObjects = _pools[itemType];
+
+        for (var i = 0; i < pooledObjects.Count; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy)
+                return pooledObjects[i];
+        }
+
+        return CreateNewObject(itemType);
+    }
+
+    private GameObject CreateNewObject(PoolItemType itemType)
+    {
+        GameObject objToCreate = null;
+        for (var i = 0; i < _objectsToPool.Count; i++)
+        {
+            if (_objectsToPool[i].ItemType == itemType)
+            {
+                objToCreate = _objectsToPool[i].Prefab;
+                break;
+            }
+        }
+
+        var newObj = Instantiate(objToCreate, transform);
+        newObj.SetActive(false);
+        _pools[itemType].Add(newObj);
+        return newObj;
+    }
+}
+
+[System.Serializable]
+public class ObjectPoolItem
+{
+    [SerializeField] private PoolItemType _itemType;
+    [SerializeField] private GameObject _prefab;
+
+    public PoolItemType ItemType
+    {
+        get { return _itemType; }
+    }
+
+    public GameObject Prefab
+    {
+        get { return _prefab; }
+    }
+}
