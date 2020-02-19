79d135637ad21dcfdf5bf801ffae2f9ad833fd02
Emanuell Amorim <amorimnuell@gmail.com>
2020-02-19 17:37:36 -0400

    Create the EnemySpawner
    Signed-off-by: Emanuell Amorim <amorimnuell@gmail.com>

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private Slider _healthSlider = default;
+    [SerializeField] private TextMeshProUGUI _healthText = default;
+    [SerializeField] private Image _healthFill = default;
+    [SerializeField] private Gradient _colorGradient = default;
+
+    private int _currentHealth, _maxHealth;
+
+    private void Awake()
+    {
+        Events.instance.AddListener<PlayerAttackEvent>(OnPlayerAttack);
+    }
+
+    private void OnDestroy()
+    {
+        Events.instance.AddListener<PlayerAttackEvent>(OnPlayerAttack);
+    }
+
+    public void SetData(EnemyData enemyData)
+    {
+        _maxHealth = enemyData.Health;
+        _healthSlider.maxValue = _maxHealth;
+        _healthSlider.value = _maxHealth;
+
+        SetCurrentHealth(enemyData.Health);
+    }
+
+    private void SetCurrentHealth(int health)
+    {
+        _currentHealth = health;
+        _healthText.text = _currentHealth.ToString();
+    }
+
+    private void OnPlayerAttack(PlayerAttackEvent e)
+    {
+        //UpdateHealth
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private EnemyDataCollection _enemyCollection = default;
+    [SerializeField] private Transform _spawnLocation = default;
+
+    private void Start()
+    {
+        //TODO: In the future if we have a list of enemies pre defined
+        //we can instantiate all of them ahead of time.
+        SpawnNewEnemy();
+    }
+
+    private void SpawnNewEnemy()
+    {
+        var enemyData = SelectEnemyFromCollection();
+        var enemyController = Instantiate(enemyData.Prefab, _spawnLocation.position, _spawnLocation.rotation);
+        enemyController.SetData(enemyData);
+    }
+
+    private EnemyData SelectEnemyFromCollection()
+    {
+        var data = _enemyCollection.GetCollection();
+        if (data.Length == 0)
+        {
+            Debug.LogError("EnemyCollection is Empty");
+            return null;
+        }
+
+        var pickId = UnityEngine.Random.Range(0, data.Length - 1);
+        return data[pickId];
+    }
+}
