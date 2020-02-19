5da6826078afea24a4e2a4f0d210380d716b8c5b
Emanuell Amorim <amorimnuell@gmail.com>
2020-02-19 17:34:26 -0400

    Create the DataCollection and EnemyDataCollection
    Signed-off-by: Emanuell Amorim <amorimnuell@gmail.com>

---
+++ Assets/Scripts/DataCollection.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataCollection<T> : MonoBehaviour where T : ICollectionItem
+{
+    [SerializeField] private T[] _data = default;
+
+    public T[] GetCollection()
+    {
+        return _data;
+    }
+
+    public List<int> GetIds()
+    {
+        var ids = new List<int>();
+
+        for (var i = 0; i < _data.Length; i++)
+            ids.Add(_data[i].GetId());
+
+        return ids;
+    }
+
+    public T GetItem(int id)
+    {
+        for (var i = 0; i < _data.Length; i++)
+        {
+            if (_data[i].GetId() == id)
+                return _data[i];
+        }
+
+        Debug.LogError($"CardDataCollection:GetData - CardId {id} not found!");
+        return default;
+    }
+}
+
+public interface ICollectionItem
+{
+    int GetId();
+}
---
+++ Assets/Scripts/EnemyData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Enemy Data", menuName = "Enemy Data")]
+public class EnemyData : ScriptableObject, ICollectionItem
+{
+    [SerializeField] private int _id = default;
+    [SerializeField] private int _health = default;
+    [SerializeField] private int _turnsToAttack = default;
+    [SerializeField] private int _attackPower = default;
+    [SerializeField] private Vector2 _gridSize = default;
+    [SerializeField] private EnemyController _prefab = default;
+
+    public int Health => _health;
+    public int TurnsToAttack => _turnsToAttack;
+    public int AttackPower => _attackPower;
+    public Vector2 GridSize => _gridSize;
+    public EnemyController Prefab => _prefab;
+
+    public int GetId()
+    {
+        return _id;
+    }
+}
