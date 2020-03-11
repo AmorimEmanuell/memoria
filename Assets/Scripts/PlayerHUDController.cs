f8f341b9b11bc5d355ec6d71e90a6921725a38aa
Emanuell Amorim <amorimnuell@gmail.com>
2020-03-11 18:59:55 -0400

    Create PlayerStatus class and remove game logic from PlayerHUDController
    Signed-off-by: Emanuell Amorim <amorimnuell@gmail.com>

---
+++ Assets/Scripts/PlayerHUDController.cs
@@ -0,0 +1,59 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerHUDController : MonoBehaviour
+{
+    [SerializeField] private Button _potionBtn = default;
+    [SerializeField] private TextMeshProUGUI _potionCount = default;
+    [SerializeField] private Slider _healthSlider = default;
+    [SerializeField] private Image _healthFill = default;
+    [SerializeField] private Gradient _healthColorGradient = default;
+
+    private const float AnimationTime = 0.5f;
+
+    public Action OnPotionButtonClicked;
+
+    private void Awake()
+    {
+        _potionBtn.onClick.AddListener(() => OnPotionButtonClicked?.Invoke());
+    }
+
+    private void OnDestroy()
+    {
+        _potionBtn.onClick.RemoveAllListeners();
+    }
+
+    public void SetInitialValues(PlayerStatus player)
+    {
+        _healthSlider.maxValue = player.Data.MaxHealth;
+        _healthSlider.value = player.Data.MaxHealth;
+
+        _healthFill.color = _healthColorGradient.Evaluate(1);
+
+        _potionCount.text = player.Data.MaxPotions.ToString();
+        _potionBtn.interactable = false;
+    }
+
+    public void UpdateHealth(int currentHealth, float healthPercentage)
+    {
+        _healthSlider.DOValue(currentHealth, AnimationTime);
+
+        var healthColor = _healthColorGradient.Evaluate(healthPercentage);
+        _healthFill.DOColor(healthColor, AnimationTime);
+    }
+
+    public void UpdatePotions(int currentPotions)
+    {
+        _potionCount.text = currentPotions.ToString();
+    }
+
+    public void ActivatePotionButton(bool active)
+    {
+        _potionBtn.interactable = active;
+    }
+}
---
+++ Assets/Scripts/PlayerStatus.cs
@@ -0,0 +1,74 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    [SerializeField] private PlayerHUDController _hudController = default;
+    [SerializeField] private Camera _camera = default;
+
+    private int _currentHealth;
+    private int _currentPotions;
+    private float HealthPercentage => _currentHealth / Data.MaxHealth;
+
+    public PlayerSaveData Data { get; private set; }
+
+    private void Awake()
+    {
+        Data = PlayerSaveData.Load();
+        _currentHealth = Data.MaxHealth;
+        _currentPotions = Data.MaxPotions;
+
+        _hudController.OnPotionButtonClicked += PotionButton_Clicked;
+    }
+
+    private void Start()
+    {
+        ResetDefaultValues();
+    }
+
+    private void OnDestroy()
+    {
+        _hudController.OnPotionButtonClicked -= PotionButton_Clicked;
+    }
+
+    public void ResetDefaultValues()
+    {
+        _hudController.SetInitialValues(this);
+    }
+
+    private void PotionButton_Clicked()
+    {
+        _currentPotions--;
+
+        _currentHealth += Data.PotionStrength;
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, Data.MaxHealth);
+
+        _hudController.UpdatePotions(_currentPotions);
+        _hudController.UpdateHealth(_currentHealth, HealthPercentage);
+        _hudController.ActivatePotionButton(ShouldActivatePotionButton());
+    }
+
+    private bool ShouldActivatePotionButton()
+    {
+        return
+            _currentHealth > 0 &&
+            _currentHealth < Data.MaxHealth &&
+            _currentPotions > 0;
+    }
+
+    public bool ApplyDamage(int damagePower)
+    {
+        _currentHealth -= damagePower;
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, Data.MaxHealth);
+
+        _hudController.UpdateHealth(_currentHealth, HealthPercentage);
+        _hudController.ActivatePotionButton(ShouldActivatePotionButton());
+
+        _camera.DOShakePosition(.2f, .05f);
+
+        return _currentHealth > 0;
+    }
+}
