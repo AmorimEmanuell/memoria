83224963b589c188141ac118ef9842e5a0b6c4b1
Emanuell Amorim <amorimnuell@gmail.com>
2020-04-05 10:47:47 -0400

    Removed unused namespace from scripts
    Signed-off-by: Emanuell Amorim <amorimnuell@gmail.com>

--- Assets/Scripts/Utils/Events/BlockCardSelectionEvent.cs
+++ Assets/Scripts/Utils/Events/BlockCardSelectionEvent.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BlockCardSelectionEvent : GameEvent
+﻿public class BlockCardSelectionEvent : GameEvent
 {
     public bool Block { get; private set; }
 
--- Assets/Scripts/Utils/Events/PlayerDefeatEvent.cs
+++ Assets/Scripts/Utils/Events/PlayerDefeatEvent.cs
@@ -1,7 +1,3 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerDefeatEvent : GameEvent
+﻿public class PlayerDefeatEvent : GameEvent
 {
 }
