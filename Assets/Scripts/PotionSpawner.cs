5024952c2aef052307413829e322a55baf13cd69
Emanuell Amorim <amorimnuell@gmail.com>
2020-04-07 18:42:31 -0400

    Implement a timer for potions to remain in the screen
    If the user does not pick a potion before a determined time it will
    disappear.
    
    Signed-off-by: Emanuell Amorim <amorimnuell@gmail.com>

--- Assets/Scripts/PotionSpawner.cs
+++ Assets/Scripts/PotionSpawner.cs
@@ -1,4 +1,5 @@
 ﻿using DG.Tweening;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,8 +17,10 @@ public class PotionSpawner : MonoBehaviour
         for (var i = 0; i < MaxActivePotions; i++)
         {
             var potion = Instantiate(potionPrefab, transform);
-            potion.OnPickup += Potion_OnPickup;
             potion.gameObject.SetActive(false);
+            potion.OnPickup += Potion_OnPickup;
+            potion.OnFadeRoutineComplete += Potion_OnFadeRoutineComplete;
+
             potions.Add(potion);
         }
     }
@@ -27,17 +30,28 @@ public class PotionSpawner : MonoBehaviour
         for (var i = 0; i < MaxActivePotions; i++)
         {
             potions[i].OnPickup -= Potion_OnPickup;
+            potions[i].OnFadeRoutineComplete -= Potion_OnFadeRoutineComplete;
         }
     }
 
     private void Potion_OnPickup(PotionPickup potion)
     {
+        DeactivatePotion(potion);
+        Events.instance.Raise(new PotionPickedEvent());
+    }
+
+    private void Potion_OnFadeRoutineComplete(PotionPickup potion)
+    {
+        DeactivatePotion(potion);
+    }
+
+    private void DeactivatePotion(PotionPickup potion)
+    {
+        potion.ResetState();
         potion.gameObject.SetActive(false);
         potions.Remove(potion);
         potions.Add(potion);
         activePotions--;
-
-        Events.instance.Raise(new PotionPickedEvent());
     }
 
     public void Spawn(Vector3 location)
@@ -47,7 +61,10 @@ public class PotionSpawner : MonoBehaviour
         potion.gameObject.SetActive(true);
 
         var direction = CalculateMoveDirection();
-        potion.transform.DOMove(location + direction, 0.5f);
+        potion.transform.DOMove(location + direction, 0.5f).OnComplete(() =>
+        {
+            potion.PrepareToFade();
+        });
     }
 
     private PotionPickup SelectAvailablePotion()
@@ -71,7 +88,7 @@ public class PotionSpawner : MonoBehaviour
 
     private Vector3 CalculateMoveDirection()
     {
-        var angle = UnityEngine.Random.value * (Mathf.PI * 2);
+        var angle = UnityEngine.Random.Range(Mathf.PI, 2 * Mathf.PI);
         return new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
     }
 }
