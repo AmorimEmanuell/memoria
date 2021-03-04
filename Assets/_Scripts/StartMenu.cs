078f441852ad68b72fd228115e2364d86c8973cd
Emanuell Amorim <amorimnuell@gmail.com>
2021-03-04 11:30:06 -0400

    Create basic start scene with simple menu
    Signed-off-by: Emanuell Amorim <amorimnuell@gmail.com>

---
+++ Assets/_Scripts/StartMenu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    [SerializeField] private Button _startButton;
+    [SerializeField] private Button _highScoreButton;
+    [SerializeField] private Button _creditsButton;
+
+    private void Awake()
+    {
+        _startButton.onClick.AddListener(OnStartButtonClicked);
+        _highScoreButton.onClick.AddListener(OnHighScoreButtonClicked);
+        _creditsButton.onClick.AddListener(OnCreditsButtonClicked);
+    }
+
+    private void OnStartButtonClicked()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnHighScoreButtonClicked()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnCreditsButtonClicked()
+    {
+        throw new NotImplementedException();
+    }
+}
