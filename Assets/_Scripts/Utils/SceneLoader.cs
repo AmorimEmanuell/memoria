e511b7fb0aeab6fef543d13f7b3c200af0b17840
Emanuell Amorim <amorimnuell@gmail.com>
2021-03-05 10:37:26 -0400

    Create the SceneLoader
    Signed-off-by: Emanuell Amorim <amorimnuell@gmail.com>

---
+++ Assets/_Scripts/Utils/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : Singleton<SceneLoader>
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine(LoadSceneInternal(sceneName));
+    }
+
+    private IEnumerator LoadSceneInternal(string sceneName)
+    {
+        var asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        asyncLoad.allowSceneActivation = true;
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
